89a4cafa5742cc35c00f71d514a2b636c9d769cd
Parth Patel <parth2002715@gmail.com>
2021-11-16 14:00:52 -0500

    angular binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ["h1 { color: red; }"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'student-list';
+  name: string = "";
+  display: boolean = true;
+  subject: any = null;
+  
+  clicked():void{
+    this.title = "Fred's student-list";
+  }
+  clicks():void{
+    this.title = "Parth's personal button";
+  }
+  ngOnInit(): void{
+    console.debug("this is a debug message");
+  }
 }
